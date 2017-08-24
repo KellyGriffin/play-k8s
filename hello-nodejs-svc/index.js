b02801a40545951e35b18788782edbc07acac7b3
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-23 20:21:00 -0500

    server created

---
+++ hello-nodejs-svc/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+var server = app.listen(3000, () => {
+  let host = server.address().address
+  let port = server.address().port
+
+  console.log('server running at http://%s:%s', host, port)
+})
+
