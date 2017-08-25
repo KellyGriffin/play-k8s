c42bf86fe1277d8e00bc6cebbf14f5d12e786f2f
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-24 19:41:10 -0500

    new version to deployment specification

--- hello-nodejs-svc/index.js
+++ hello-nodejs-svc/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello Bro, What is going?')
 })
 
 var server = app.listen(3000, () => {
